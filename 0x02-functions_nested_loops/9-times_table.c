c0123cdbd907cb886fe3d043a702bab7582e7ccf
Suku20 <sitholesukoluhle@gmail.com>
2022-10-19 03:08:07 -0700

    9times-table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,9 +19,9 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
-				-putchar(' ');
+				_putchar(' ');
 			else
-				-putchar((prod / 10) + '0');
+				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
