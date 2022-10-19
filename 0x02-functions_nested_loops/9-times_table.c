cad78ef88ccb0460db0b1fe2c59d801e5fe6c537
Suku20 <sitholesukoluhle@gmail.com>
2022-10-19 03:27:06 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/***
  * times_table - print the 9 times table
  */
 void times_table(void)
@@ -22,7 +22,8 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
