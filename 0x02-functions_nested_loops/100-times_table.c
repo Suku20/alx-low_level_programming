527ac63f2a58c0d5b12195ae4879cc866e491d48
Suku20 <sitholesukoluhle@gmail.com>
2022-10-19 17:42:33 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints_times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
 void print_times_table(int n)
@@ -41,7 +41,7 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				}
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
