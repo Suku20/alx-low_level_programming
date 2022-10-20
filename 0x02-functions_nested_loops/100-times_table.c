6d06394009295e3106ba553cdeb1837e85c780df
Suku20 <sitholesukoluhle@gmail.com>
2022-10-19 17:34:21 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,13 +30,13 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((k / 10 + '0');
+					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((k / 100 + '0');
+					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
@@ -44,5 +44,5 @@ void print_times_table(int n)
 			_putchar('\n')
 		}
 	}
-
 }
+
